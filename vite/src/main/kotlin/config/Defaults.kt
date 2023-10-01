1205abccd91baad64cdd68431bc7ae6f382cf78b
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-10-01 15:31:19 +0200

    fix(vite): Incorrect import of the @originjs/vite-plugin-commonjs plugin

--- vite/src/main/kotlin/config/Defaults.kt
+++ vite/src/main/kotlin/config/Defaults.kt
@@ -9,7 +9,7 @@ internal fun ViteConfig.defaultConfigurationFor(project: Project) {
 	root.convention(buildRoot.dir("kotlin"))
 	base.convention(buildRoot)
 
-	plugin("@originjs/vite-plugin-commonjs", "viteCommonJs", "1.0.3", isNamedExport = true)
+	plugin("@originjs/vite-plugin-commonjs", "viteCommonjs", "1.0.3", isNamedExport = true)
 	plugin("@rollup/plugin-commonjs", "commonjs", "24.0.1")
 
 	// Build
