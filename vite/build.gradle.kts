d9f0d7653463ca6ad61ebb9b42d44428c3afa672
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-10-08 10:58:10 +0200

    build(gradle): Add the required Central metadata to the plugin marker

--- vite/build.gradle.kts
+++ vite/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("conventions.library")
 
 	`kotlin-dsl`
+	`java-gradle-plugin`
 }
 
 repositories {
@@ -26,4 +27,16 @@ library {
 	name.set("Vite for Kotlin")
 	description.set("Gradle plugin to use Vite instead of Webpack in Kotlin Multiplatform projects")
 	homeUrl.set("https://gitlab.com/opensavvy/kotlin-vite")
+}
+
+afterEvaluate {
+	tasks.named("generatePomFileForVitePluginMarkerMavenPublication", GenerateMavenPom::class.java) {
+		pom.name.set("Vite for Kotlin")
+		pom.description.set("Plugin marker for the Gradle plugin Vite for Kotlin")
+		pom.url.set("https://gitlab.com/opensavvy/kotlin-vite")
+
+		pom.scm {
+			url.set("https://gitlab.com/opensavvy/kotlin-vite")
+		}
+	}
 }
