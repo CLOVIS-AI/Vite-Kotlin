7e4873a7be6ee91369d29fc6afe40f9015102bd4
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-10-28 19:55:50 +0200

    refactor(vite-base): Extract the ViteExec task to Base

--- vite-kotlin/src/main/kotlin/tasks/ViteExecTask.kt
+++ vite-kotlin/src/main/kotlin/tasks/ViteExecTask.kt
@@ -1,121 +1,54 @@
 package opensavvy.gradle.vite.kotlin.tasks
 
-import opensavvy.gradle.vite.kotlin.*
-import org.gradle.api.DefaultTask
+import opensavvy.gradle.vite.base.tasks.ViteExec
+import opensavvy.gradle.vite.kotlin.KotlinVitePlugin
+import opensavvy.gradle.vite.kotlin.viteBuildDevDir
+import opensavvy.gradle.vite.kotlin.viteBuildDistDir
+import opensavvy.gradle.vite.kotlin.viteBuildProdDir
 import org.gradle.api.Project
 import org.gradle.api.file.RegularFile
-import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.provider.ListProperty
-import org.gradle.api.provider.Property
-import org.gradle.api.tasks.*
-import org.gradle.process.ExecOperations
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin.Companion.kotlinNodeJsExtension
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmProject
 import java.io.File
-import javax.inject.Inject
 
-/**
- * Executes a Vite command in the context of this project.
- */
-@Suppress("LeakingThis")
-@CacheableTask
-abstract class ViteExecTask @Inject constructor(
-	private val process: ExecOperations,
-) : DefaultTask() {
+private fun ViteExec.execConventions() {
+	dependsOn(":kotlinNpmInstall")
+	group = KotlinVitePlugin.GROUP
 
-	/**
-	 * The Vite command to execute, for example `run` or `build`.
-	 */
-	@get:Input
-	@get:Optional
-	abstract val command: Property<String>
+	val kotlinEnvironment = project.rootProject.kotlinNodeJsExtension
+	nodePath.set(
+		kotlinEnvironment
+			.requireConfigured()
+			.let { RegularFile { File(it.nodeExecutable) } }
+	)
 
-	/**
-	 * Arguments passed to [command].
-	 */
-	@get:Input
-	abstract val arguments: ListProperty<String>
-
-	/**
-	 * The path to the Node executable.
-	 *
-	 * Use this property to override which Node installation is used.
-	 * By default, the Node installation used by the Kotlin plugin is used.
-	 */
-	@get:InputFile
-	@get:PathSensitive(PathSensitivity.RELATIVE)
-	abstract val nodePath: RegularFileProperty
-
-	/**
-	 * The path to the Vite executable.
-	 *
-	 * Use this property to override which Vite installation is used.
-	 * By default, Vite is installed as a development dependency of the project in which this plugin is applied.
-	 */
-	@get:InputFile
-	@get:PathSensitive(PathSensitivity.RELATIVE)
-	abstract val vitePath: RegularFileProperty
-
-	@get:Input
-	abstract val workingDirectory: Property<String>
-
-	init {
-		group = KotlinVitePlugin.GROUP
-		description = "Executes a given Vite command"
-
-		arguments.convention(emptyList())
-
-		val kotlinEnvironment = project.rootProject.kotlinNodeJsExtension
-		nodePath.convention(
-			kotlinEnvironment
-				.requireConfigured()
-				.let { RegularFile { File(it.nodeExecutable) } }
-		)
-
-		vitePath.convention(
-			kotlinEnvironment
-				.projectPackagesDir.parentFile.resolve(NpmProject.NODE_MODULES)
-				.let { RegularFile { File("$it/vite/bin/vite.js") } }
-		)
-
-		val config = project.kotlinViteExtension
-		inputs.property("vite_version", config.version)
-	}
-
-	@TaskAction
-	fun execute() {
-		process.exec {
-			commandLine(
-				nodePath.get().asFile.absolutePath,
-				vitePath.get().asFile.absolutePath,
-				command.getOrElse(""),
-				"-c",
-				"../vite.config.js",
-				*arguments.get().toTypedArray(),
-			)
-
-			workingDir(workingDirectory.get())
-		}
-	}
-
+	vitePath.set(
+		kotlinEnvironment
+			.projectPackagesDir.parentFile.resolve(NpmProject.NODE_MODULES)
+			.let { RegularFile { File("$it/vite/bin/vite.js") } }
+	)
 }
 
 internal fun createExecTasks(project: Project) {
-	project.tasks.register("viteBuild", ViteExecTask::class.java) {
+	project.tasks.register("viteBuild", ViteExec::class.java) {
 		description = "Builds the production variant of the project"
-		dependsOn("viteConfigureProd", "viteCompileProd", ":kotlinNpmInstall")
+		dependsOn("viteConfigureProd", "viteCompileProd")
+		execConventions()
 
 		command.set("build")
 
-		workingDirectory.set(project.viteBuildProdDir.map { "$it/kotlin" })
+		config.root.set(project.viteBuildProdDir.map { it.dir("kotlin") })
+		configurationFile.set(project.viteBuildProdDir.map { it.file("vite.config.js") })
 		outputs.dir(project.viteBuildDistDir)
 	}
 
-	project.tasks.register("viteRun", ViteExecTask::class.java) {
+	project.tasks.register("viteRun", ViteExec::class.java) {
 		description = "Hosts the development variant of the project"
-		dependsOn("viteConfigureDev", "viteCompileDev", ":kotlinNpmInstall")
+		dependsOn("viteConfigureDev", "viteCompileDev")
+		execConventions()
 
-		workingDirectory.set(project.viteBuildDevDir.map { "$it/kotlin" })
+		config.root.set(project.viteBuildDevDir.map { it.dir("kotlin") })
+		configurationFile.set(project.viteBuildDevDir.map { it.file("vite.config.js") })
 	}
 
 	project.tasks.named("assemble") {
