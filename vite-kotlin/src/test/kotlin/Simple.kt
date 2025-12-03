62ffcf8c1d87017f66bd3632562688a063d07f4c
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-12-03 18:36:52 +0100

    test(kotlin): Add a ES2015 test

--- vite-kotlin/src/test/kotlin/Simple.kt
+++ vite-kotlin/src/test/kotlin/Simple.kt
@@ -70,4 +70,65 @@ val Simple by preparedSuite(preparedConfig = CoroutineTimeout(10.minutes)) {
 		check(dist().listDirectoryEntries().map { it.name }.sorted() == listOf("assets", "index.html"))
 	}
 
+	test("Simple example with es2015") {
+		simpleBuild()
+
+		val indexHtml = app().dir / "src" / "jsMain" / "resources" / "index.html"
+		val main = app().dir / "src" / "jsMain" / "kotlin" / "Main.kt"
+		val dist = app().buildDir / "vite" / "dist"
+
+		// language="kotlin"
+		app().buildKts("""
+			plugins {
+				kotlin("multiplatform")
+				id("dev.opensavvy.vite.kotlin")
+			}
+			
+			kotlin {
+				js {
+					browser()
+					binaries.executable()
+					useEsModules()
+
+					compilerOptions {
+						target.set("es2015")
+					}
+				}
+			}
+		""".trimIndent())
+
+		// language="HTML"
+		indexHtml().createParentDirectories()
+		indexHtml().writeText("""
+			<!DOCTYPE html>
+			<html lang="end">
+			<head>
+				<meta charset="UTF-8">
+			</head>
+			<body>
+				<div id="root"></div>
+				<script src="gradle-test-app.mjs" type="module"></script>
+			</body>
+			</html>
+		""".trimIndent())
+
+		// language="kotlin"
+		main().createParentDirectories()
+		main().writeText("""
+			fun main() {
+				println("Hello world!")
+			}
+		""".trimIndent())
+
+		val result = gradle.runner()
+			.withArguments("app:viteBuild")
+			.withPluginClasspath()
+			.build()
+
+		println(result.output)
+
+		check(dist().exists())
+		check(dist().listDirectoryEntries().map { it.name }.sorted() == listOf("assets", "index.html"))
+	}
+
 }
