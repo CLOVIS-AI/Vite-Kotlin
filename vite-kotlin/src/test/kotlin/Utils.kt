e6b99fbbc0c3c2d50e0cecf7b2ef456f2be6b260
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-12-03 18:23:50 +0100

    test(kotlin): Add a basic test

---
+++ vite-kotlin/src/test/kotlin/Utils.kt
@@ -0,0 +1,30 @@
+package opensavvy.gradle.vite.kotlin
+
+import opensavvy.prepared.compat.gradle.buildKts
+import opensavvy.prepared.compat.gradle.gradle
+import opensavvy.prepared.compat.gradle.settingsKts
+import opensavvy.prepared.suite.prepared
+
+fun createBuild(vararg projectName: String) = prepared {
+	gradle.settingsKts("""
+		rootProject.name = "gradle-test"
+		
+		dependencyResolutionManagement {
+			repositories {
+				mavenCentral()
+			}
+		}
+
+		include(${projectName.joinToString { "\"$it\"" }})
+	""".trimIndent())
+
+	gradle.rootProject.buildKts("""
+		plugins {
+			kotlin("multiplatform") apply false
+		}
+		
+		project.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin> {
+			project.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsEnvSpec>().version = "24.11.1"
+		}
+	""".trimIndent())
+}
